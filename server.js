cf97ff9244004e684ab84d2b8adad4af3639c0e9
Chayce Knaub <chknaub@yahoo.com>
2020-06-09 00:21:46 -0400

    started port

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require("express");
 const morgan = require("morgan");
-
+// creates instance of express function
 const app = express();
-
+// remember morgan is logging requests to
+//terminal console
 app.use(morgan("dev"));
 
-app.use((req, res) => {
-  res.send("Hello, world!");
-});
+// app.use((req, res) => {
+//   res.send("Hello, world!");
+// });
 
 const PORT = 8000;
+
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`,
+];
+
+app.get("/types", handleGetTypes);
+
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
