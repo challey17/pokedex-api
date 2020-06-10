7a3e93de8f1414028abd208bfc907039d5a39a7e
Chayce Knaub <chknaub@yahoo.com>
2020-06-09 23:24:47 -0400

    pokemon go bitches

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const POKEDEX = require("./pokedex.json");
 
 console.log(process.env.API_TOKEN);
 
@@ -12,7 +13,13 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get("Authorization");
   console.log("validate bearer token middleware");
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized request" });
+  }
   // move to the next middleware
   next();
 });
@@ -48,7 +55,24 @@ function handleGetTypes(req, res) {
 }
 
 function handleGetPokemon(req, res) {
-  res.send("Hello Pokemon");
+  let response = POKEDEX.pokemon;
+
+  // filter our pokemon by name if name query param is present
+  if (req.query.name) {
+    response = response.filter((pokemon) =>
+      // case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+
+  // filter our pokemon by type if type query param is present
+  if (req.query.type) {
+    response = response.filter((pokemon) =>
+      pokemon.type.includes(req.query.type)
+    );
+  }
+
+  res.json(response);
 }
 
 app.listen(PORT, () => {
