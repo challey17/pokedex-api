64272e198bd3eca063ceb230d5c31d97bc645644
Chayce Knaub <chknaub@yahoo.com>
2020-06-10 22:10:20 -0400

    procfile ready to deploy to heroku

--- server.js
+++ server.js
@@ -77,6 +77,16 @@ function handleGetPokemon(req, res) {
   res.json(response);
 }
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
